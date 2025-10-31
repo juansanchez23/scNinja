0054aa507a9bc8b001eccbe13af852a639590de0
juansanchez23 <s23rodriguez2004@gmail.com>
2025-10-31 08:20:26 -0500

    Se decide crear aparte un repositorio en NodeJs para trabajar el front, se crea el archivo CorsConfig.java para configurar los Cors

---
+++ src/main/java/com/proyectoNinja/scNinja/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.proyectoNinja.scNinja.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Aplica CORS a todos los endpoints bajo /api
+                        .allowedOrigins("http://localhost:3000") // Permite solicitudes desde tu frontend React
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos los encabezados
+                        .allowCredentials(true); // Si vas a usar cookies o autenticación basada en sesiones
+            }
+        };
+    }
+}
